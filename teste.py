3ab35c156b73806a409d30ebf975c65c1a4c30ec
Tiberio Luiz <tiberiolc@gmail.com>
2026-05-05 09:45:48 -0400

    conclusao do projeto

---
+++ teste.py
@@ -0,0 +1,20 @@
+
+
+
+if __name__ == '__main__':
+    x = [1,2,3]
+    y = None
+    z=list(filter(lambda t: t == 72, x))
+
+
+    if z:
+        print ("Z é verdadeiro")
+    else:
+        print ("Z é falso") 
+
+    if y is not None:
+        print ("Y é verdadeiro")
+    else:
+        print ("Y é falso")      
+
+
