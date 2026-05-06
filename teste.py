10bf0f1abb5ad752b405521772147220e307f2a3
Tiberio Luiz <tiberiolc@gmail.com>
2026-05-06 11:11:06 -0400

    inclusao de testea do projeto

--- teste.py
+++ teste.py
@@ -5,14 +5,6 @@ BASE_URL = "http://localhost:5000"
 tasks=[]
 
 
-def test_listar():
-    response = requests.get(f"{BASE_URL}/listar")
-    assert response.status_code == 200
-    data = response.json()
-    assert "tasks" in data
-    assert "total_tasks" in data
-    assert "completed" in data
-    
 def test_create_task():
     payload = {
         "name": "tarefa de teste",
@@ -23,6 +15,35 @@ def test_create_task():
     assert response.status_code == 201
     data = response.json()
     assert data["message"] == "Tarefa criada com sucesso"
+
+
+def test_listar():
+    response = requests.get(f"{BASE_URL}/listar")
+    assert response.status_code == 200
+    data = response.json()
+    tarefas = data["tasks"]
+    print(tarefas)
+    assert "tasks" in data
+    assert "total_tasks" in data
+    assert "completed" in tarefas[0]
+    
+    
+
+def test_listar2():
+    response = requests.get(f"{BASE_URL}/listar")
+    data = response.json()
+    tarefas = data["tasks"]
+    print ("type de response: ", type(response)  )
+    print ("type de data: ", type(data  )  )
+    print ("type de tarefas: ", type(tarefas)  )
+    tarefa = tarefas[0]
+    print ("type de tarefa: ", type(tarefa)  )
+    print(tarefa)
+
+
+if __name__ == '__main__':
+    print ("Iniciando testes...")
+    test_listar2()
 
 # if __name__ == '__main__':
 #     x = [1,2,3]
